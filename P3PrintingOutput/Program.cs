edf4b255cca83721f513cfd3832ef8b1e1792ba3
adamviger <75254051+adamviger@users.noreply.github.com>
2023-10-24 15:40:44 +0200

    Project 3

--- P3PrintingOutput/Program.cs
+++ P3PrintingOutput/Program.cs
@@ -1,3 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello World!");
+// Output: Hello World!
+Console.WriteLine("Welcome to 102 CSharp Basics!");
+// Output: Welcome to 102 CSharp Basics!
+Console.Write("It's easy to print output ");
+Console.Write("to the console!");
+// Output: It's easy to print output to the console!
