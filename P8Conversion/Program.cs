6dee14fd03cc68368bd097158ecdee47dcd9c12f
adamviger <75254051+adamviger@users.noreply.github.com>
2023-10-26 12:25:09 +0200

    Project 8

---
+++ P8Conversion/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Give me a number:");
+string number = Console.ReadLine();
+double conversion = double.Parse(number);
+Console.WriteLine(conversion);
+int i = (int)conversion;
+Console.WriteLine(i);
+int original = int.Parse(number);
+Console.WriteLine(original);
