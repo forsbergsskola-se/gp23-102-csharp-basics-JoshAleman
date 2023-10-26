0e02d4a8500173b1bda3acc5fe72c3cca7633948
adamviger <75254051+adamviger@users.noreply.github.com>
2023-10-26 11:02:54 +0200

    Project 6

---
+++ P6DataTypes/Program.cs
@@ -0,0 +1,7 @@
+﻿string outputString;
+outputString = "Hello World";
+Console.WriteLine(outputString); // This prints outputString
+float decimalValue = 0.3f;
+Console.WriteLine(decimalValue); // This prints decimalValue
+bool isAwesome = true;
+Console.WriteLine(isAwesome); // This prints isAwesome 
