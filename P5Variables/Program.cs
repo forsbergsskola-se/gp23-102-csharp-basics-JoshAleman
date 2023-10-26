c2df20abfa7b6b931f8190fddc03ded8560a5ba3
adamviger <75254051+adamviger@users.noreply.github.com>
2023-10-26 10:31:08 +0200

    Project 5

---
+++ P5Variables/Program.cs
@@ -0,0 +1,11 @@
+﻿string a;
+a = "Hello";
+Console.WriteLine(a); // This prints a
+a = "World"; 
+Console.WriteLine(a); // This prints a
+string b;
+b = "World"; 
+Console.WriteLine(b); // This prints b
+a = "Forsbergs";
+Console.WriteLine(a); // This prints a
+Console.WriteLine(b); // This prints b
