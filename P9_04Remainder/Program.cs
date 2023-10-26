454198202fdd669471a005b581f479d5ee0636b3
adamviger <75254051+adamviger@users.noreply.github.com>
2023-10-26 16:23:03 +0200

    Project 9

--- P9_04Remainder/Program.cs
+++ P9_04Remainder/Program.cs
@@ -1,1 +1,8 @@
 ﻿// P09_04Remainder:
+Console.WriteLine("Enter two numbers");
+Console.WriteLine("Enter number 1:");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter number 2:");
+int number2 = int.Parse(Console.ReadLine());
+float result = (float)number1 % number2;
+Console.WriteLine($"That would be {result}.");
