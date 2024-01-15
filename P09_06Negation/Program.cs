b0fe00a5da1af69cd9b7d387f728f5c839fa35ad
JoshAleman <148553327+JoshAleman@users.noreply.github.com>
2024-01-15 16:11:41 +0100

    Project 9

---
+++ P09_06Negation/Program.cs
@@ -0,0 +1,6 @@
+﻿// P09_06Negation
+Console.WriteLine("Write a number to make it negative:");
+int number1 = int.Parse(Console.ReadLine());
+float f = number1;
+float g = -f;
+Console.WriteLine($"That would be {g}");
